7d2a544a7dec70ca79d12cdd8508ca35d3f64317
Matt Gefen <matt.gefen@gmail.com>
2024-08-19 16:32:38 -0600

    profile sizing and page size variant for tablet

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,13 @@ export default function Home() {
     <main>
       <div className={`${styles.hero}`}>
         <div className={`${styles.hero_left}`}>
-          <Image src={ProfileIcon} alt={"Matt Icon"} width={100} height={100} className={`${styles.hero_profile}`}/>
+          <Image src={ProfileIcon} alt={"Matt Icon"} width={150} height={150} className={`${styles.hero_profile}`}/>
         </div>
         <div className={`${styles.hero_right}`}>
           <div className={`${styles.hero_text}`}>
             <h1>Matt Gefen</h1>
             <h2>Full-Stack Engineer</h2>
-          </div>
-          <div className={`${styles.social_links}`}>
+            <div className={`${styles.social_links}`}>
             <Link href={'https://github.com/matt-gefen'} target="_blank" className={`${styles.social_link}`}>
               <Github/>
               Github
@@ -27,19 +26,26 @@ export default function Home() {
               <LinkedIn/>
               LinkedIn
             </Link>
-            {/* <Link href={'mailto: matt.gefen@gmail.com'} target="_blank" className={`${styles.social_link}`}>
+            <Link href={'mailto: matt.gefen@gmail.com'} target="_blank" className={`${styles.social_link}`}>
               <Mail/>
               Mail
-            </Link> */}
+            </Link>
+          </div>
           </div>
         </div>
       </div>
-      <div>
+      <div className={`${styles.main_body}`}>
         <p>
-          {`Based in Boulder, Colorado with a passion for learning and problem solving. I'm currently working at Roadtrip Nation to build accessible, educational web-based experiences for students looking for career guidance. Talk to me about hiking, books, video games, and D&D.`}
+          Hi, I'm Matt Gefen! I'm a  developer, a lifelong learner, and a passionate problem solver. I currently work at <span><Link className={`${styles.embedded_link}`} href={'roadtripnation.com'}>Roadtrip Nation</Link></span> as a Full-Stack Engineer using Typescript, Next.js, and MongoDB.
+        </p>
+        <p>
+          I create accessible and educational experiences using best-in-class video content. These tools enable students looking for passion in their careers to find guidance fit for them.
+        </p>
+        <p>
+          Talk to me about tabletop games, books, video-games, and the outdoors!
         </p>
       </div>
-      <div>Landing Page Footer</div>
     </main>
   );
 }
+// to build accessible, educational experiences for students looking for career guidance.
