318486433f654440f8c06c9e529a4bbcf301e693
Matt Gefen <matt.gefen@gmail.com>
2024-09-04 10:25:07 -0600

    photos and conditional nav social links

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import styles from './NavBar.module.css'
 import { usePathname } from 'next/navigation'
 import { map } from 'lodash';
+import Github from '../icons/Github';
+import LinkedIn from '../icons/LinkedIn';
 
 export default function NavBar() {
   const routes = [
@@ -20,14 +22,32 @@ export default function NavBar() {
     },
   ]
   const pathName = usePathname();
-  
+
   return (
     <div className={`${styles.nav_bar}`}>
-      {
-        map(routes, (route, index)=> {
+      <div className={`${styles.nav_bar_links}`}>
+        {
+          map(routes, (route, index) => {
             return <Link href={route.url} className={pathName === route.url ? `${styles.active_route}` : ''} key={index}>{route.name}</Link>
-        })
+          })
+        }
+      </div>
+      {
+        pathName !== '/' && (
+          <div className={`${styles.social_links}`}>
+            <Link href={'https://github.com/matt-gefen'} target="_blank" className={`${styles.social_link}`}>
+              <Github />
+            </Link>
+            <Link href={'https://www.linkedin.com/in/mattgefen/'} target="_blank" className={`${styles.social_link}`}>
+              <LinkedIn />
+            </Link>
+            {/* <Link href={'mailto: matt.gefen@gmail.com'} target="_blank" className={`${styles.social_link}`}>
+                  <Mail/>
+                </Link> */}
+          </div>
+        )
       }
+
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -28,28 +28,31 @@ export default function Home() {
             <Link href={'https://www.linkedin.com/in/mattgefen/'} target="_blank" className={`${styles.social_link}`}>
               <LinkedIn/>
             </Link>
-            {/* <Link href={'mailto: matt.gefen@gmail.com'} target="_blank" className={`${styles.social_link}`}>
-              <Mail/>
-            </Link> */}
-          </div>
+            </div>
           </div>
         </div>
       </div>
       <div className={`${styles.main_body}`}>
         <p>
-          {"Hi, I\'m Matt Gefen! I\'m a developer currently working at "}<span><Link className={`${styles.embedded_link}`} href={'roadtripnation.com'}>Roadtrip Nation</Link></span> crafting impactful software.
+          Hi, I&apos;m Matt Gefen! I&apos;m a developer living in Boulder, CO currently working at <span><Link className={`${styles.embedded_link}`} href={'roadtripnation.com'}>Roadtrip Nation</Link></span> crafting impactful software.
         </p>
         <p>
           I create accessible and educational experiences using best-in-class video content. These tools enable students looking for passion in their careers to find guidance fit for them.
         </p>
         <p>
-          When I&apos;m not working, I&apos;m likely outside with my partner Olivia and our dog Basil, playing a TTRPG, or reading a book.
+          When I&apos;m not working, I&apos;m likely outside with my partner Olivia and our dog Basil, playing a tabletop game, or reading a book.
         </p>
-        <p>
-          I'd love to talk to you about technology, tabletop games, fantasy books, video-games, and the outdoors!
-        </p>
+        {/* <p>
+         I'd love to talk to you about technology, tabletop games, fantasy books, video-games, and the outdoors!
+        </p> */}
+        <p>Reach out here: <Link className={`embedded_link`} href={'mailto:matt.gefen@gmail.com'} target="_blank">matt.gefen@gmail.com</Link></p>
+        <div className={`${styles.image_container}`}>
+          <div className={`${styles.image_portrait}`} style={{backgroundImage: "url(https://1u4j97n0laxnso2y.public.blob.vercel-storage.com/IMG_0979-dy2hRgg2FFhZLO5O2xxkCGqfdfdN8L.png)"}}/>
+          <div className={`${styles.image_portrait}`} style={{backgroundImage: "url(https://1u4j97n0laxnso2y.public.blob.vercel-storage.com/IMG_4906-RUBVCAMlUqdymgv6f3ldVLgl6NH8bo.png)"}}/>
+          <div className={`${styles.image_portrait}`} style={{backgroundImage: "url(https://1u4j97n0laxnso2y.public.blob.vercel-storage.com/IMG_8246-xJuE3IPkRHGDxb4QAsGAsoxxBuSkEa.png)"}}/>
+          <div className={`${styles.image_portrait}`} style={{backgroundImage: "url(https://1u4j97n0laxnso2y.public.blob.vercel-storage.com/IMG_8164-k5xGDKVuYIghCJ5EbJ9o0nYSb3zWOO.png)"}}/>
+        </div>
       </div>
     </main>
   );
 }
-// to build accessible, educational experiences for students looking for career guidance.
